f2be81b9b5c133243f8055c7999cc5b07b78debd
Hrithik GAIKWAD <ghrithik53@gmail.com>
2021-12-30 18:38:58 +0530

    a

--- ritesh/urls.py
+++ ritesh/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from django.conf import settings, url
+from django.conf import settings
 from django.conf.urls import static
 from django.views.static import serve
-
+from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('',include('k2.urls')),
+    path('',include('k2.urls')),
      
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
